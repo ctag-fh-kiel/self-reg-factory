1c799268a439cb9b66e5de7e95ed27bace37856c
Robert Manzke <robert.manzke@fh-kiel.de>
2020-01-04 21:23:47 +0400

    library doesn't work due to linker

--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@
 using factory::Registry;
 
 int main(int argc, char** argv) {
-    std::unique_ptr<ctagAudioPlugin> plugin = Registry<ctagAudioPlugin>::New(argv[1]);
+    std::unique_ptr<ctagAudioPlugin> plugin = Registry<ctagAudioPlugin>::New("ctagAudioPluginAM");
     assert(plugin);
     float in[32], out[32];
-    plugin->Process(in, out);  // will draw a rectangle!
-    plugin = Registry<ctagAudioPlugin>::New(argv[2]);
-    plugin->Process(in, out); 
+    plugin->Process(in, out);  
     plugin = nullptr;
 }
